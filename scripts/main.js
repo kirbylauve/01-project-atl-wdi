fe8002311355a7443fb3deaeac0eabac5a872cbf
Kirby Lockard <kirby@Kirbys-MacBook-Pro.local>
2019-02-15 22:43:44 -0500

    trying to make hdn-wrd change from transparent to visible

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,18 @@ console.log("js working");
 const alphabetArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"];
 const hiddenWord = ["ARRAY", "OBJECT", "FUNCTION", "VARIABLE"]
 var hdnArr = []
+var word = ""
 
 
 //function to check if clicked button is in the word
 const checkButton = function () {
-    console.log(word)
     if (word.indexOf(this.innerHTML) > -1) { //checking if the letter is in the word by refrencing the the index values of the letters in the string tha is the current word. -1 means it not there by making 0 truthy
-    console.log('hw');
-}
+        /* $(".hdnLtr").attr('color', 'black') */  //this is changing ALL the hdnLtr divs to color=black
+        $( div.contains(`${this.innerHTML}`) ).css( "color", "black" );
+        console.log('console.log(this)', this) //<button>Letter</button>
+        console.log('this.innerHTML', this.innerHTML) // Letter
+        console.log('word.indexOf(this.innerHTML)', word.indexOf(this.innerHTML)) //index # of letter
+    }
 }
 
 // function to make letter buttons
@@ -24,23 +28,25 @@ function makeLetters() {
         alphaContainer.appendChild(buttons); //adds new button into the correct area
     }
 }
-
 makeLetters()
-var word = ""
+
+
 //function to pick from list of words and place divs in hdn-wrd area
-    function pickWord() {
-        word = hiddenWord[Math.floor(Math.random() * hiddenWord.length)];
-        hdnArr = word.split("");
-        for (var i = 0; i < hdnArr.length; i++) {
-            var newLtr = hdnArr[i];
-            const ltrToGuess = document.createElement('div');
-            ltrToGuess.setAttribute('class','hdnLtr');
-            ltrToGuess.innerHTML = newLtr;
-            const hdnWrdContainer = document.querySelector('.hdn-wrd');
-            hdnWrdContainer.appendChild(ltrToGuess);
-            console.log(ltrToGuess)
-        }
-      
+function pickWord() {
+    word = hiddenWord[Math.floor(Math.random() * hiddenWord.length)];
+    hdnArr = word.split("");
+    for (var i = 0; i < hdnArr.length; i++) {
+        var newLtr = hdnArr[i];
+        const ltrToGuess = document.createElement('div'); // ltrToGuess = new div created
+        ltrToGuess.setAttribute('class', `hdnLtr`);
+        /* ltrToGuess.setAttribute('id', `${hdnArr[i]}`); */  //gives div id of its letter but if I use id then I'd have to make a style for each one
+        ltrToGuess.innerHTML = newLtr;
+        const hdnWrdContainer = document.querySelector('.hdn-wrd');
+        hdnWrdContainer.appendChild(ltrToGuess);
+        console.log('ltrToGuess', ltrToGuess);
+        console.log('newLtr', newLtr)
     }
-    pickWord()
+
+}
+pickWord()
 
