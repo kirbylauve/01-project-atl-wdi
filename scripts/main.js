3d4f92d3fa80bdf4e3a3f07153f722994b160920
Kirby Lockard <kirby@Kirbys-MacBook-Pro.local>
2019-02-17 17:53:59 -0500

    defined all functions, only player two gets to play...

--- scripts/main.js
+++ scripts/main.js
@@ -7,15 +7,15 @@ var word;
 var wrdArr = []
 var crctLtrArr = []
 const wrngLtrArr = []
-const losses = []
+const pTwoScore = []
 var crctPikArr = []
-const wins = []
+var pOneScore = []
+var plays = 2;
 
 
+//const playGame = function() {
 
-
-
-const checkButton = function () {
+ const checkButtonPOne = function () {
     if (word.indexOf(this.innerHTML) > -1) { //if the letter exists as an index value in the word
         let ltrInBtn = this.innerHTML; // = letter value of button clicked
         let hdnToVisByClass = document.querySelectorAll('.hdnLtr'); //gets all elements with class .hdnLtr
@@ -33,12 +33,13 @@ const checkButton = function () {
         crctLtrArr = [...new Set(wrdArr)]; //eliminates duplicate letters
         if (crctLtrArr.length === crctPikArr.length) { //if simplified word array length === correctly guessed letters length
             alert(`The word was ${word} you win!`); //alert win
-            wins.push('win'); //add win to wins array
-            document.getElementById('winner').innerHTML = `${wins.length}`; //update text to equal length of wins array
+            pOneScore.push('win'); //add win to wins array
+            document.getElementById('playerOneScore').innerHTML = `${pOneScore.length}`; //update text to equal length of wins array
             hiddenWord.splice(hiddenWord.indexOf(word), 1) //remove word from list 
             crctPikArr.length = 0 //reset correct guesses
             wrngLtrArr.length = 0 //reset incorrect guesses
             resetBoard() //reset buttons and pick new word
+            plays++
 
         }
 
@@ -50,30 +51,91 @@ const checkButton = function () {
             alert('you have lost!');
             wrngLtrArr.length = 0; //reset length to zero
             crctPikArr.length = 0 //reset good guesses
-            losses.push('loss'); // add a loss to the loss array
-            document.getElementById('loser').innerHTML = `${losses.length}`; //update loss tally to = number length of array
+            pTwoScore.push('loss'); // add a loss to the loss array
+            document.getElementById('playerTwoScore').innerHTML = `${pTwoScore.length}`; //update loss tally to = number length of array
             resetBoard();   //replace letters with new and pick new word
+            plays++
         }
     }
-    this.removeEventListener('click', checkButton);
+    this.removeEventListener('click', checkButtonPOne);
 }
 
+
+const checkButtonPTwo = function () {
+    if (word.indexOf(this.innerHTML) > -1) { //if the letter exists as an index value in the word
+        let ltrInBtn = this.innerHTML; // = letter value of button clicked
+        let hdnToVisByClass = document.querySelectorAll('.hdnLtr'); //gets all elements with class .hdnLtr
+        let hdnToVisArr = Array.from(hdnToVisByClass); //turns node list into array
+        hdnToVisArr.forEach(function (elem) { //cycles through new array. elem is the div of the hidden letter
+            if (elem.dataset.letter === `${ltrInBtn}`) { //if the data-letter value = innerHTML of button pressed
+                elem.classList.remove('hdnLtr'); //remove hidden class
+                elem.classList.add('visible'); //add visible class
+            }
+
+        })
+        this.classList.add('alph-btn-good'); //change class of button to good
+        crctPikArr.push(this.innerHTML); //pushes letter into crctPikArr
+        wrdArr = Array.from(word) //changes word to be guessed into array
+        crctLtrArr = [...new Set(wrdArr)]; //eliminates duplicate letters
+        if (crctLtrArr.length === crctPikArr.length) { //if simplified word array length === correctly guessed letters length
+            alert(`The word was ${word} you win!`); //alert win
+            pTwoScore.push('win'); //add win to wins array
+            document.getElementById('playerTwoScore').innerHTML = `${pTwoScore.length}`; //update text to equal length of wins array
+            hiddenWord.splice(hiddenWord.indexOf(word), 1) //remove word from list 
+            crctPikArr.length = 0 //reset correct guesses
+            wrngLtrArr.length = 0 //reset incorrect guesses
+            resetBoard() //reset buttons and pick new word
+            plays++
+
+        }
+
+    } else {
+        this.classList.add('alph-btn-bad'); //change class of button to bad
+        alert('letter not in word');
+        wrngLtrArr.push(this.innerHTML); //push the letter that is incorrect into an array wrngLtrArr
+        if (wrngLtrArr.length >= 6) { //if the lelngth of array reaches 6
+            alert('you have lost!');
+            wrngLtrArr.length = 0; //reset length to zero
+            crctPikArr.length = 0 //reset good guesses
+            pTwoScore.push('loss'); // add a loss to the loss array
+            document.getElementById('playerOneScore').innerHTML = `${pOneScore.length}`; //update loss tally to = number length of array
+            resetBoard();   //replace letters with new and pick new word
+            plays++
+        }
+    }
+    this.removeEventListener('click', checkButtonPTwo);
+}
+
+
 // function to make letter buttons
-function makeLetters() {
+const makeLettersOne = function() {
+    alert("go player one")
     for (var i = 0; i < alphabetArr.length; i++) {
         var newBtn = alphabetArr[i]; // index value is assigned to variable newBtn
         const buttons = document.createElement('button'); //use .createElement to produce functional button and assign to variable button
         buttons.innerHTML = newBtn; //set text of the button 
         const alphaContainer = document.querySelector('.alphabet'); //sets location for new buttons to go to
-        buttons.addEventListener('click', checkButton); // adds event listener to buttons
+        buttons.addEventListener('click', checkButtonPOne); // adds event listener to buttons
         alphaContainer.appendChild(buttons); //adds new button into the correct area
     }
 }
-makeLetters()
+
+const makeLettersTwo = function() {
+    alert("go player two");
+    for (var i = 0; i < alphabetArr.length; i++) {
+        var newBtn = alphabetArr[i]; // index value is assigned to variable newBtn
+        const buttons = document.createElement('button'); //use .createElement to produce functional button and assign to variable button
+        buttons.innerHTML = newBtn; //set text of the button 
+        const alphaContainer = document.querySelector('.alphabet'); //sets location for new buttons to go to
+        buttons.addEventListener('click', checkButtonPTwo); // adds event listener to buttons
+        alphaContainer.appendChild(buttons); //adds new button into the correct area
+    }
+}
+
 
 
 //function to pick from list of words and place divs in hdn-wrd area
-function pickWord() {
+const pickWord = function() {
     word = hiddenWord[Math.floor(Math.random() * hiddenWord.length)];
     hdnArr = word.split("");
     for (var i = 0; i < hdnArr.length; i++) {
@@ -93,12 +155,35 @@ function pickWord() {
 pickWord()
 
 //function to get new word and alphabet after game is complete
-function resetBoard() {
+const resetBoard = function() {
     $(".alphabet").empty(); //find div with class alphabet and remove all children elements
     $(".hdn-wrd").empty();
     pickWord();
-    makeLetters();
+    newPlayer();
+    
 }
+
+//} tried to wrap everything into a play game function
+
+
+const startGame = function() {
+    newPlayer()
+    document.querySelector(".startBtn").removeEventListener('click', startGame)
+    console.log("start")
+   // playGame();
+    /* if (hiddenWord.length === 0) {
+        if (playeronescore > playertwoscore) {
+            alert("player one pOneScore")
+        } else if ( playertwoscore > playeronescore) {
+            alert("player two wins")
+        }
+        else {
+            alert("it's a tie");
+        }
+    } */
+}
+
+
 
 
 //on click start
@@ -111,4 +196,13 @@ function resetBoard() {
 //  on resetBoard() 
 //  switch player...
 //      if numPlaysArr.length % 2 === 0 { go player 1}
-//      if numPLaysArr.length % 2 === 1 { go player 2}
+/* //      if numPLaysArr.length % 2 === 1 { go player 2} */
+document.querySelector(".startBtn").addEventListener('click', startGame)
+
+const newPlayer = function() {
+    if (plays.length % 2 === 0) {
+    makeLettersOne()
+} else {
+    makeLettersTwo()
+}
+}
